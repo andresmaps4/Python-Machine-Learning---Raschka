2e46980e62f797da210c90daa423cb9f214b69dc
Andres Ramirez <amramirezp@rcnradio.com.co>
2023-10-24 15:49:20 -0500

    cambio

--- Codes/Chapter 3/LogisticRegression.py
+++ Codes/Chapter 3/LogisticRegression.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 class LogisticRegressionGD:
     """Gradient descent-based logistic regression classifier.
@@ -24,4 +24,53 @@ class LogisticRegressionGD:
 
     """
 
+    def __init__(self, eta=0.01 , n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the
+            number of examples and n_features is the
+            number of features.
+        y : array-like, shape = [n_examples]
+            Target values.
+
+        Returns
+        -------
+        self : Instance of LogisticRegressionGD
+        """
     
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[0])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input) # activation(net_input) viene a ser mi sigma(z(i))
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (-y.dot(np.log(output))
+                    -((1 - y).dot(np.log(1-output)))
+                    / X.shape[0])
+            self.losses_.append(loss)
+        return self
+    
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+    
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1.0)  
